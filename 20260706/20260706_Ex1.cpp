ae28a591dc6e2ec3389cd24a64832444826cb447
OSHIBA115 <ohina482@gmail.com>
2026-07-06 09:52:23 +0900

    例題追加

--- 20260706/20260706_Ex1.cpp
+++ 20260706/20260706_Ex1.cpp
@@ -2,7 +2,22 @@
 
 using namespace std;
 
+void Add(int &num)
+{
+	num += 10;
+}
+
 int main(void)
 {
+	int number;
+
+	cout << "”š‚ğ“ü—Í‚µ‚Ä‚­‚¾‚³‚¢B" << endl;
+
+	cin >> number;
+
+	Add(number);
+
+	cout << number << "‚Å‚·B" << endl;
+
 	return 0;
 }
