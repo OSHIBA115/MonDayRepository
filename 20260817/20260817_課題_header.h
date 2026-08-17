eb4c2d4a1fea36617aafcdfe68b2be3d6c4917d3
senahomura <ohina482@gmail.com>
2026-08-17 12:18:15 +0900

    途中

---
+++ 20260817/20260817_課題_header.h
@@ -0,0 +1,4 @@
+#pragma once
+
+//関数プロトタイプ宣言
+void StartGame();
---
+++ 20260817/20260817_課題_main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include "20260817_‰Û‘è_header.h"
+
+using namespace std;
+
+int main()
+{
+	StartGame();
+	return 0;
+}
