9e718b0f8ecf7aa665b91a78a323ecc9ba4b0ef0
Roll-006 <ohina482@gmail.com>
2026-08-24 12:04:58 +0900

    演習問題４完了

---
+++ 20260824/Project1/20240824_Prac4_橋冨陽心.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+
+using namespace std;
+
+void Multiples(int* pnum, int elements)
+{
+	int multiples;//倍数保存
+
+	cin >> multiples;//倍数入力
+
+	for (int i = 0; i < elements; i++)//要素数繰り返し
+	{
+		pnum[i] = pnum[i] * multiples;//計算して保存
+
+		cout << *(pnum + i) << endl;//表示
+	}
+
+}
+
+int main(void)
+{
+	//定数
+	const int NUM_MAX = 5;//numbersの配列数
+
+	int numbers[NUM_MAX] = { 10,20,30,40,50 };//配列numbersを初期化
+	int* pnum;//ポインタ変数pnumを初期化
+
+	pnum = numbers;//ポインタ変数pnumに配列numbersの先頭の番地を代入
+
+	Multiples(pnum,NUM_MAX);//n倍する処理
+
+	return 0;//終了
+}
